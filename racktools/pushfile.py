ea72df291ba54e7da3039a7ba941040c2bacf022
Carl Scharenberg <carl.scharenberg@gmail.com>
2011-03-25 11:53:14 -0500

    wrapped pushfile into container

--- racktools/pushfile.py
+++ racktools/pushfile.py
@@ -1,16 +1,54 @@
+#!/bin/python
+
 import cloudfiles
+import mimetypes
+import glob
+
+"""
 cloud_user = MY_CLOUD_USER
 cloud_key = MY_CLOUD_KEY
 cloud_container = 'FILES'
+"""
 
-conn =  cloudfiles.get_connection(cloud_user, cloud_key)
-container = conn.get_container(cloud_container)
+class Connector(object):
+	def __init__(self, user, api_key, cloud_container):
+		self.user = user
+		self.api_key = key
+		self.cloud_container = cloud_container
+		self.conn = cloudfiles.get_connection(self.user, self.api_key)
+		self.container = self.conn.get_container(cloud_container)
 
-# open a file and write the descriptor
-fd = open(FILENAME,'rb')
-ob = container.create_object('name of my file') # you can use fd.name to pull it from the file descriptor
-ob.content_type = 'application/zip'
-# you can use the save(fd) method if the source is changing and you want to provide a stream
-ob.write(fd)
-fd.close()
+	def getContentType(self, source):
+		return mimetypes.guess_type(source)
+
+	def push(self, source, target='AUTO', content_type='AUTO'):
+		if target.lower()=='auto':
+			target = source
+		if content_type.lower() == 'auto':
+			content_type = self.getContentType(source)
+	
+		# open a file and write the descriptor
+		fd = open(source,'rb')
+		ob = self.container.create_object(target) # you can use fd.name to pull it from the file descriptor
+		ob.content_type = content_type
+		# you can use the save(fd) method if the source is changing and you want to provide a stream
+		ob.write(fd)
+		fd.close()
+
+
+if __name__=='__main__':
+	import sys
+	user = sys.argv[1]
+	key = sys.argv[2]
+	container = sys.argv[3]
+	file_ptns = sys.argv[4:]
+
+	manifest = []
+	for ptn in file_ptns:
+		files = glob.glob(ptn)
+		manifest.extend(files)
+
+	connector = Connector(user, key, container)
+	for file in manifest:
+		connector.push(file)	
 
